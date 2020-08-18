069414dd75859075d6f3ead36d806a1ddfc4d226
goze <goze.qiu@gmail.com>
2020-08-18 15:50:24 +0800

    simple demo for 1v1

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# no
+# WED
+
+websocket to debug
+
+![](https://github.com/gozeon/wsd/blob/master/Screen Shot 2020-08-18 at 15.45.12.png)
 
 # referece
 
--- main.js
+++ main.js
@@ -7,28 +7,25 @@ const wss = new WebSocket.Server({
 })
 
 wss.on('connection', function (ws, req) {
-  // 给他个表示别
+  // 给他个标识
   ws.id = req.headers['sec-websocket-key']
-  console.log(ws)
 
   ws.on('message', function (message) {
     wss.clients.forEach((w) => {
-      console.log(w.id)
+      // 转发除了自己的所有连接
+      if (w.id !== ws.id) {
+        w.send(message)
+      }
     })
-    ws.send(message)
-
-    console.log(ws.id, JSON.stringify(message))
-  })
-
-  ws.on('pong', function () {
-    console.log('pong')
   })
 })
 
-wss.on('close', function (ws) {})
+server.get('/dashboard', (req, res) => {
+  res.sendFile(__dirname + '/dashboard.html')
+})
 
-server.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+server.get('/demo', (req, res) => {
+  res.sendFile(__dirname + '/demo.html')
 })
 
 server.listen(3000)
